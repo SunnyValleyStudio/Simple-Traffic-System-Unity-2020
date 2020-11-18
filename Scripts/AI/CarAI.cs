f5417ff1656ab63ce56fc624cf22489b2afb1fba
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-11-18 13:01:10 +0100

    Video 6 - Car AI

---
+++ Scripts/AI/CarAI.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CarAI : MonoBehaviour
+{
+    [SerializeField]
+    private List<Vector3> path = null;
+    [SerializeField]
+    private float arriveDistance = .3f, lastPointArriveDistance = .1f;
+    [SerializeField]
+    private float turningAngleOffset = 5;
+    [SerializeField]
+    private Vector3 currentTargetPosition;
+
+    private int index = 0;
+
+    private bool stop;
+
+    public bool Stop
+    {
+        get { return stop; }
+        set { stop = value; }
+    }
+
+    [field: SerializeField]
+    public UnityEvent<Vector2> OnDrive { get; set; }
+
+    private void Start()
+    {
+        if(path == null || path.Count == 0)
+        {
+            Stop = true;
+        }
+        else
+        {
+            currentTargetPosition = path[index];
+        }
+    }
+
+    public void SetPath(List<Vector3> path)
+    {
+        if(path.Count == 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        this.path = path;
+        index = 0;
+        currentTargetPosition = this.path[index];
+
+        Vector3 relativepoint = transform.InverseTransformPoint(this.path[index + 1]);
+
+        float angle = Mathf.Atan2(relativepoint.x, relativepoint.z) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, angle, 0);
+        Stop = false;
+    }
+
+    private void Update()
+    {
+        CheckIfArrived();
+        Drive();
+    }
+
+    private void Drive()
+    {
+        if (Stop)
+        {
+            OnDrive?.Invoke(Vector2.zero);
+        }
+        else
+        {
+            Vector3 relativepoint = transform.InverseTransformPoint(currentTargetPosition);
+            float angle = Mathf.Atan2(relativepoint.x, relativepoint.z) * Mathf.Rad2Deg;
+            var rotateCar = 0;
+            if(angle > turningAngleOffset)
+            {
+                rotateCar = 1;
+            }else if(angle < -turningAngleOffset)
+            {
+                rotateCar = -1;
+            }
+            OnDrive?.Invoke(new Vector2(rotateCar, 1));
+        }
+    }
+
+    private void CheckIfArrived()
+    {
+        if(Stop == false)
+        {
+            var distanceToCheck = arriveDistance;
+            if(index == path.Count - 1)
+            {
+                distanceToCheck = lastPointArriveDistance;
+            }
+            if(Vector3.Distance(currentTargetPosition,transform.position) < distanceToCheck)
+            {
+                SetNextTargetIndex();
+            }
+        }
+    }
+
+    private void SetNextTargetIndex()
+    {
+        index++;
+        if(index >= path.Count)
+        {
+            Stop = true;
+            Destroy(gameObject);
+        }
+        else
+        {
+            currentTargetPosition = path[index];
+        }
+    }
+}
