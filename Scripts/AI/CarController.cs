137f27cf65d3fb426e09159e84de5a3d3a760da0
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-11-18 12:26:28 +0100

    Video 5 - Car prefab P2

---
+++ Scripts/AI/CarController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CarController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField]
+    private float power = 5;
+    [SerializeField]
+    private float torque = 0.5f;
+    [SerializeField]
+    private float maxSpeed = 5;
+
+    [SerializeField]
+    private Vector2 movementVector;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector2 movementInput)
+    {
+        this.movementVector = movementInput;
+    }
+
+    private void FixedUpdate()
+    {
+        if(rb.velocity.magnitude < maxSpeed)
+        {
+            rb.AddForce(movementVector.y * transform.forward * power);
+        }
+        rb.AddTorque(movementVector.x * Vector3.up * torque * movementVector.y);
+    }
+
+}
