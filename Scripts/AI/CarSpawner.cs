5de01aa71cce7d72eb3314fde88830f9f823f397
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-11-18 12:04:13 +0100

    Video 4 - Car prefab P1

---
+++ Scripts/AI/CarSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject[] carPrefabs;
+
+    private void Start()
+    {
+        Instantiate(SelectACarPrefab(), transform);
+    }
+
+    private GameObject SelectACarPrefab()
+    {
+        var randomIndex = Random.Range(0, carPrefabs.Length);
+        return carPrefabs[randomIndex];
+    }
+}
