e3d6a5ad7b3f43bf9eed73421a6bf5fb6207ef36
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-11-19 11:40:47 +0100

    Video 8 - Car markers p1

---
+++ Scripts/Marker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace SimpleCity.AI
+{
+    public class Marker : MonoBehaviour
+    {
+        public Vector3 Position { get => transform.position;}
+
+        public List<Marker> adjacentMarkers;
+
+        [SerializeField]
+        private bool openForConnections;
+
+        public bool OpenForconnections
+        {
+            get { return openForConnections; }
+        }
+
+        public List<Vector3> GetAdjacentPositions()
+        {
+            return new List<Vector3>(adjacentMarkers.Select(x => x.Position).ToList());
+        }
+
+        private void OnDrawGizmos()
+        {
+            if(Selection.activeObject == gameObject)
+            {
+                Gizmos.color = Color.red;
+                if (adjacentMarkers.Count > 0)
+                {
+                    foreach (var item in adjacentMarkers)
+                    {
+                        Gizmos.DrawLine(transform.position, item.Position);
+                    }
+                }
+                Gizmos.color = Color.white;
+            }
+        }
+    }
+
+}
