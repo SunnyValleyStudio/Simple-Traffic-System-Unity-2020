1fc26bc0d9c3afc3c13bf8c1310eec3255dea44b
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-11-19 11:19:25 +0100

    Fixed asset issue 2

--- Scripts/AiDirector.cs
+++ Scripts/AiDirector.cs
@@ -1,53 +1,52 @@
-﻿//	Created by: Sunny Valley Studio 
-//	https://svstudio.itch.io
-
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace SVS
+namespace SimpleCity.AI
 {
-
     public class AiDirector : MonoBehaviour
     {
         public PlacementManager placementManager;
-        //public RoadManager roadManager;
         public GameObject[] pedestrianPrefabs;
 
         public GameObject carPrefab;
 
         AdjacencyGraph graph = new AdjacencyGraph();
-        List<Vector3> pathForPedestrian = new List<Vector3>();
+
+        public void SpawnAllAagents()
+        {
+            foreach (var house in placementManager.GetAllHouses())
+            {
+                TrySpawningAnAgent(house, placementManager.GetRandomSpecialStrucutre());
+            }
+            foreach (var specialStructure in placementManager.GetAllSpecialStructures())
+            {
+                TrySpawningAnAgent(specialStructure, placementManager.GetRandomHouseStructure());
+            }
+        }
 
         private void TrySpawningAnAgent(StructureModel startStructure, StructureModel endStructure)
         {
-            
-            if (startStructure != null)
+            if(startStructure != null && endStructure != null)
             {
-                var roadStartPosition = ((INeedingRoad)startStructure).RoadPosition;
-                var spawnPoint = ((RoadStructure)placementManager.GetStructureAt(roadStartPosition)).GetPedestrianSpawnMarker(startStructure.transform.position).Position;
-                var agent = Instantiate(GetRandomPedestrian(), spawnPoint, Quaternion.identity);
-                agent.transform.position = spawnPoint;
-                if (endStructure != null)
-                {
-                    var roadEndPosition = ((INeedingRoad)endStructure).RoadPosition;
-                    var endPoint = ((RoadStructure)placementManager.GetStructureAt(roadEndPosition)).GetPedestrianSpawnMarker(endStructure.transform.position).Position;
-                    var path = placementManager.GetPathBetween(roadStartPosition, roadEndPosition, true);
-                    if (path.Count > 2)
-                    {
-                        path.Reverse();
-                        List<Vector3> pedestrianPath = GetPedestrianPathFromRoadPath(path, spawnPoint, endPoint);
-                        var aiAgent = agent.GetComponent<AIAgent>();
-                        aiAgent.Initailize(pedestrianPath);
-                        return;
-                    }
-                }
-                Destroy(agent);
-                Debug.LogError("Agent has no destination or it cannot reach it!!!");
-            }
+                var startPosition = ((INeedingRoad)startStructure).RoadPosition;
+                var endPosition = ((INeedingRoad)endStructure).RoadPosition;
 
+                var startMarkerPosition = placementManager.GetStructureAt(startPosition).GetPedestrianSpawnMarker(startStructure.transform.position);
+                var endMarkerPosition = placementManager.GetStructureAt(endPosition).GetNearestMarkerTo(endStructure.transform.position);
+
+                var agent = Instantiate(GetRandomPedestrian(), startMarkerPosition.Position, Quaternion.identity);
+                var path = placementManager.GetPathBetween(startPosition, endPosition, true);
+                if(path.Count > 0)
+                {
+                    path.Reverse();
+                    List<Vector3> agentPath = GetPedestrianPath(path, startMarkerPosition.Position, endMarkerPosition);
+                    var aiAgent = agent.GetComponent<AiAgent>();
+                    aiAgent.Initialize(agentPath);
+                }
+            }
         }
 
         public void SpawnACar()
@@ -60,7 +59,7 @@ namespace SVS
 
         private void TrySpawninACar(StructureModel startStructure, StructureModel endStructure)
         {
-            if(startStructure != null && endStructure != null)
+            if (startStructure != null && endStructure != null)
             {
                 var startRoadPosition = ((INeedingRoad)startStructure).RoadPosition;
                 var endRoadPosition = ((INeedingRoad)endStructure).RoadPosition;
@@ -73,67 +72,49 @@ namespace SVS
             }
         }
 
-        public void SpawnAllAgents()
-        {
-            foreach (var house in placementManager.GetAllHouses())
-            {
-                TrySpawningAnAgent(house, placementManager.GetRandomSpecialStrucutre());
-            }
-            foreach (var house in placementManager.GetAllSpecialStructures())
-            {
-                TrySpawningAnAgent(house, placementManager.GetRandomHouseStructure());
-            }
-        }
-
-        private GameObject GetRandomPedestrian()
-        {
-            return pedestrianPrefabs[UnityEngine.Random.Range(0, pedestrianPrefabs.Length)];
-        }
-
-        public List<Vector3> GetPedestrianPathFromRoadPath(List<Vector3Int> path, Vector3 startStructurePosition, Vector3 endStructurePosition)
+        private List<Vector3> GetPedestrianPath(List<Vector3Int> path, Vector3 startPosition, Vector3 endPosition)
         {
             graph.ClearGraph();
-            CreateAGraph(path);
-            pathForPedestrian = AdjacencyGraph.AStarSearch(graph, startStructurePosition, endStructurePosition);
-            return pathForPedestrian;
+            CreatAGraph(path);
+            Debug.Log(graph);
+            return AdjacencyGraph.AStarSearch(graph,startPosition,endPosition);
         }
 
-        private void CreateAGraph(List<Vector3Int> path)
+        private void CreatAGraph(List<Vector3Int> path)
         {
             Dictionary<Marker, Vector3> tempDictionary = new Dictionary<Marker, Vector3>();
+
             for (int i = 0; i < path.Count; i++)
             {
-                var currenPosition = path[i];
-                var roadStructure = ((RoadStructure)placementManager.GetStructureAt(currenPosition));
+                var currentPosition = path[i];
+                var roadStructure = placementManager.GetStructureAt(currentPosition);
                 var markersList = roadStructure.GetPedestrianMarkers();
-                bool limitDistance = markersList.Count == 4 ? true : false;
+                bool limitDistance = markersList.Count == 4;
                 tempDictionary.Clear();
-                foreach (Marker marker in markersList)
+                foreach (var marker in markersList)
                 {
                     graph.AddVertex(marker.Position);
-                    foreach (var markerNeighbourPosition in marker.GetAdjacentMarkers())
+                    foreach (var markerNeighbourPosition in marker.GetAdjacentPositions())
                     {
                         graph.AddEdge(marker.Position, markerNeighbourPosition);
                     }
-                    if (marker.OpenForConnections && i + 1 < path.Count)
-                    {
-                        var nextRoadStructure = ((RoadStructure)placementManager.GetStructureAt(path[i + 1]));
 
+                    if(marker.OpenForconnections && i+1 < path.Count)
+                    {
+                        var nextRoadStructure = placementManager.GetStructureAt(path[i + 1]);
                         if (limitDistance)
                         {
                             tempDictionary.Add(marker, nextRoadStructure.GetNearestMarkerTo(marker.Position));
                         }
                         else
                         {
-                            
                             graph.AddEdge(marker.Position, nextRoadStructure.GetNearestMarkerTo(marker.Position));
                         }
-
                     }
                 }
-                if (limitDistance && tempDictionary.Count == 4)
+                if(limitDistance && tempDictionary.Count == 4)
                 {
-                    var distanceSortedMarkers = tempDictionary.OrderBy(x => Vector3.Distance(x.Key.Position, x.Value)).Select(x => x).ToList();
+                    var distanceSortedMarkers = tempDictionary.OrderBy(x => Vector3.Distance(x.Key.Position, x.Value)).ToList();
                     for (int j = 0; j < 2; j++)
                     {
                         graph.AddEdge(distanceSortedMarkers[j].Key.Position, distanceSortedMarkers[j].Value);
@@ -142,41 +123,31 @@ namespace SVS
             }
         }
 
-        private void DegubGraph()
+        private GameObject GetRandomPedestrian()
+        {
+            return pedestrianPrefabs[UnityEngine.Random.Range(0, pedestrianPrefabs.Length)];
+        }
+
+        private void Update()
         {
             foreach (var vertex in graph.GetVertices())
             {
-                foreach (var vertexNeighbour in graph.GetConnectedVerticesTo(vertex.Position))
+                foreach (var vertexNeighbour in graph.GetConnectedVerticesTo(vertex))
                 {
                     Debug.DrawLine(vertex.Position + Vector3.up, vertexNeighbour.Position + Vector3.up, Color.red);
                 }
             }
         }
 
-        private void Update()
+        public void NewMethod(int parameter)
         {
-            DegubGraph();
+            Debug.Log("hello");
+        }
 
-            if(Input.GetKeyDown(KeyCode.P)){
-                var startStructure = placementManager.GetRandomHouseStructure();
-                var endStructure = placementManager.GetRandomSpecialStrucutre();
-                var roadStartPosition = ((INeedingRoad)startStructure).RoadPosition;
-                var spawnPoint = ((RoadStructure)placementManager.GetStructureAt(roadStartPosition)).GetPedestrianSpawnMarker(startStructure.transform.position).Position;
-                var agent = Instantiate(GetRandomPedestrian(), spawnPoint, Quaternion.identity);
-                agent.transform.position = spawnPoint;
-                if (endStructure != null)
-                {
-                    var roadEndPosition = ((INeedingRoad)endStructure).RoadPosition;
-                    var path = placementManager.GetPathBetween(roadStartPosition, roadEndPosition, true);
-                    if (path.Count > 0)
-                    {
-                        path.Reverse();
-                        var aiAgent = agent.GetComponent<AIAgent>();
-                        aiAgent.Initailize(new List<Vector3>(path.Select(x => (Vector3)x).ToList()));
-                        return;
-                    }
-                }
-            }
+        public void NewMethdTow(string name)
+        {
+            Debug.Log(name);
         }
     }
 }
+
